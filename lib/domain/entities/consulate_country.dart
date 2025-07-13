6158491f88cbcaa0ef6204a2f12b0f82d1d68d8b
kharoldcanova <kharoldsamir@gmail.com>
2025-07-13 12:39:18 -0500

    fix: consulate country model

--- lib/domain/entities/consulate_country.dart
+++ lib/domain/entities/consulate_country.dart
@@ -11,9 +11,9 @@ class ConsulateCountry {
 
   factory ConsulateCountry.fromJson(Map<String, dynamic> json) =>
       ConsulateCountry(
-        id: json["id"],
-        abbrev: json["abbrev"],
-        name: json["name"],
+        id: json["id"] ?? 0,
+        abbrev: json["abbrev"] ?? "",
+        name: json["name"] ?? "",
       );
 
   Map<String, dynamic> toJson() => {"id": id, "abbrev": abbrev, "name": name};
